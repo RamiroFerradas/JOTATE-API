ea73ab59558a2b50fe099fcafc0fe957e7ce13fc
Ramiro <ramiferra97@gmail.com>
2024-01-14 12:05:28 -0300

    deploy vercel

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import app from "./src/app";
 import { sequelize } from "./src/db/db";
 
 sequelize.sync({ alter: true }).then(() => {
-  const PORT = process.env.PORT ? process.env.PORT : 3001;
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, async () => {
     console.log(`Server listening at port: ${PORT}`);
   });
