f9494c7d0b3b4b4b357d5a9e9e64dfa2e1f867f3
Ramiro <ramiferra97@gmail.com>
2024-01-15 09:56:45 -0300

    fix

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,8 +5,6 @@ import { insertProducts } from "./inserProductsDb";
 dotenv.config();
 
 const { DATABASE_URL, NODE_ENV } = process.env;
-console.log(NODE_ENV);
-
 const sequelize = new Sequelize(`${DATABASE_URL}`, {
   logging: false,
   native: false,
