954765211038792be5f7d7ab0c1ecba552d245c0
Ramiro <ramiferra97@gmail.com>
2024-01-15 23:45:17 -0300

    fix vercel 3.1

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import app from "../src/app";
 import { sequelize } from "../src/db/db";
 
-sequelize.sync({ alter: true }).then(() => {
-  const PORT = process.env.PORT || 3001;
-  app.listen(PORT, async () => {
-    console.log(`Server listening at port: ${PORT}`);
-  });
+// sequelize.sync({ alter: true }).then(() => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, async () => {
+  console.log(`Server listening at port: ${PORT}`);
 });
+// });
